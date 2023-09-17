fbeeead3b2b263399a300b1261af82ce322463f0
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-17 19:09:43 +0400

    Update MinecraftESCFix.java

--- src/main/java/de/vepexlegit/minecraftescfix/MinecraftESCFix.java
+++ src/main/java/de/vepexlegit/minecraftescfix/MinecraftESCFix.java
@@ -8,10 +8,12 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.Logger;
 import org.lwjgl.input.Keyboard;
 
-@Mod(modid = MinecraftESCFix.MODID, version = MinecraftESCFix.VERSION)
+@Mod(modid = MinecraftESCFix.MODID, name = MinecraftESCFix.NAME, version = MinecraftESCFix.VERSION)
 public class MinecraftESCFix {
 
     public static Minecraft mc = Minecraft.getMinecraft();
@@ -21,23 +23,31 @@ public class MinecraftESCFix {
     }
 
     public static final String MODID = "minecraftescfix";
+    public static final String NAME = "MinecraftESCFix";
     public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
     @SubscribeEvent
     public void onGuiOpen(GuiScreenEvent.KeyboardInputEvent.Pre event) {
-        if (event.gui instanceof GuiScreenServerList && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
+        if (event.getGui() instanceof GuiScreenServerList && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
             mc.displayGuiScreen(new GuiMultiplayer(null));
         }
-        if (event.gui instanceof GuiScreenAddServer && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
+        if (event.getGui() instanceof GuiScreenAddServer && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
             mc.displayGuiScreen(new GuiMultiplayer(null));
         }
-        if (event.gui instanceof GuiCreateWorld && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
-            mc.displayGuiScreen(new GuiSelectWorld(null));
+        if (event.getGui() instanceof GuiCreateWorld && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
+            mc.displayGuiScreen(new GuiWorldSelection(null));
         }
     }
 }

0d9a446748b027a3417ebd6c5731f8f0e2fa9cad
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-27 17:34:42 +0400

    Update MinecraftESCFix.java

--- src/main/java/de/vepexlegit/minecraftescfix/MinecraftESCFix.java
+++ src/main/java/de/vepexlegit/minecraftescfix/MinecraftESCFix.java
@@ -3,10 +3,8 @@ package de.vepexlegit.minecraftescfix;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.*;
-import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
+import net.minecraft.client.multiplayer.GuiConnecting;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import org.lwjgl.input.Keyboard;
@@ -21,23 +19,20 @@ public class MinecraftESCFix {
     }
 
     public static final String MODID = "minecraftescfix";
-    public static final String VERSION = "1.0";
-
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
-    }
+    public static final String VERSION = "1.2";
 
     @SubscribeEvent
     public void onGuiOpen(GuiScreenEvent event) {
         if (event.gui instanceof GuiScreenServerList && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
             mc.displayGuiScreen(new GuiMultiplayer(null));
-        }
-        if (event.gui instanceof GuiScreenAddServer && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
+        } else if (event.gui instanceof GuiScreenAddServer && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
             mc.displayGuiScreen(new GuiMultiplayer(null));
-        }
-        if (event.gui instanceof GuiCreateWorld && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
+        } else if (event.gui instanceof GuiCreateWorld && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
             mc.displayGuiScreen(new GuiSelectWorld(null));
+        } else if (event.gui instanceof GuiScreenRealmsProxy && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
+            mc.displayGuiScreen(new GuiMainMenu());
+        } else if (event.gui instanceof GuiConnecting && Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_ESCAPE) {
+            mc.displayGuiScreen(new GuiMultiplayer(null));
         }
     }
 }
